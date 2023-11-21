6ba1c3970acace810a5966d083dced487eabaf13
prominvit <vitalii.katowice@gmail.com>
2023-11-21 18:30:27 +0100

    uzb

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import HomeGroup from './components/HomeGroup';
 import HomeGroupEng from './components/HomeGroupEng';
+import HomeGroupUZB from './components/HomeGroupUZB';
 import HomeMedia from './components/HomeMedia';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/groups" element={<HomeGroup />} />
         <Route exact path="/media" element={<HomeMedia />} />
         <Route exact path="/eng_group" element={<HomeGroupEng />} />
+        <Route exact path="/uz_group" element={<HomeGroupUZB />} />
       </Routes>
     </div>
   );
---
+++ src/components/HomeGroupUZB.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// import { Link } from 'react-router-dom';
+import logo from '../logo.png';
+import Contact from './Contact';
+
+const HomeGroupUZB = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <Link className='nav_lang' to='/pl'>PL</Link> */}
+        <Contact link={'https://t.me/uz_news_promin'} name={'Янгиликлар'} />
+        <Contact link={'https://t.me/uz_news_promin/17'} name={'iTaxi'} />
+        <Contact link={'https://t.me/events_uz'} name={'Конкурслари'} />
+        <Contact link={'https://t.me/eng_promin'} name={'Ҳайдовчилар чати'} />
+        <Contact link={'https://t.me/Promin_couriers'} name={'Курьерлар чати'} />
+        <Contact link={'https://t.me/Arenda_Promin'} name={'Автомобил ижараси'} />
+        <Contact link={'https://t.me/BaraholkaPromin'} name={'Бозор'} />
+        <Contact link={'https://t.me/rower_promin'} name={'Велосипед ва скутер ижараси'} />
+        <Contact link={'https://t.me/promin_sell_car'} name={'Автомобил олиб/сотиш'} />
+        <Contact link={'https://t.me/Black_list_pp'} name={'Қора рўйхат'} />
+        <Contact link={'https://t.me/Oblawa_ITD'} name={'Oblawa ITD'} />
+      </header>
+    </div>
+  )
+}
+
+export default HomeGroupUZB
